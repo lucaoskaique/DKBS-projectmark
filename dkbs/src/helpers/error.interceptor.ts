555823782ad7c645351c31ce3d343fcb6e91b162
lucaoskaique <lucas.kaique175@hotmail.com>
2024-09-05 21:36:22 -0300

    feat: Add class-transformer dependency

---
+++ dkbs/src/helpers/error.interceptor.ts
@@ -0,0 +1,33 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  HttpException,
+} from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+@Injectable()
+export class ErrorInterceptor implements NestInterceptor {
+  constructor() {}
+  async intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Promise<Observable<any>> {
+    return next.handle().pipe(
+      catchError((err) => {
+        return throwError(
+          () =>
+            new HttpException(
+              {
+                message: err?.message || err?.detail || 'Something went wrong',
+                timestamp: new Date().toISOString(),
+                code: err?.code || '',
+              },
+              err.statusCode || 500,
+            ),
+        );
+      }),
+    );
+  }
+}
