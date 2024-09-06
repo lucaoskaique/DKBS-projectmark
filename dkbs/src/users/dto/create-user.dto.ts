c9e2d85f1093a6ba85f9fd685ebb25db9d8a31c0
lucaoskaique <lucas.kaique175@hotmail.com>
2024-09-06 13:33:19 -0300

    refactor: Remove unused Topic entity and add Topic type

--- dkbs/src/users/dto/create-user.dto.ts
+++ dkbs/src/users/dto/create-user.dto.ts
@@ -1,1 +1,10 @@
-export class CreateUserDto {}
+import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+}
--- dkbs/src/users/users.service.ts
+++ dkbs/src/users/users.service.ts
@@ -1,26 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { DrizzleService } from 'src/database/drizzle.service';
+import { users } from '../database/database-schema';
+import { eq } from 'drizzle-orm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly drizzleService: DrizzleService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const createdUser = await this.drizzleService.db
+      .insert(users)
+      .values({
+        name: createUserDto.name,
+        email: createUserDto.email,
+      })
+      .returning();
+
+    return createdUser[0];
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    return await this.drizzleService.db.select().from(users).execute();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    const user = await this.drizzleService.db
+      .select()
+      .from(users)
+      .where(eq(users.id, id))
+      .execute();
+
+    if (!user[0]) {
+      throw new NotFoundException('No users found');
+    }
+
+    return user[0];
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const updatedUser = await this.drizzleService.db
+      .update(users)
+      .set({
+        name: updateUserDto.name,
+        email: updateUserDto.email,
+      })
+      .where(eq(users.id, id))
+      .returning();
+
+    return updatedUser;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    const deletedUser = await this.drizzleService.db
+      .delete(users)
+      .where(eq(users.id, id))
+      .returning();
+
+    return deletedUser;
   }
 }
